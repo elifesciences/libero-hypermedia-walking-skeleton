496d8957945a4aaa150ad9c8d27b5d2097769fe4
Giorgio Sironi <g.sironi@elifesciences.org>
2019-10-10 10:55:58 +0100

    Include check for RegisterAction

--- back-end/src/routes/register.ts
+++ back-end/src/routes/register.ts
@@ -5,6 +5,7 @@ import {Document, JsonLdArray, JsonLdObj} from 'jsonld/jsonld-spec';
 import {Nodes} from '../nodes';
 import Router from 'koa-router';
 
+// maybe this should be unified with post-action.ts?
 interface RegisterRouteContext extends Koa.Context {
     request: Request & {
         body: Document,
@@ -14,9 +15,14 @@ interface RegisterRouteContext extends Koa.Context {
 export default (articles: Nodes, router: Router): Koa.Middleware => {
     return async ({request, response}: RegisterRouteContext): Promise<void> => {
         const expanded = <JsonLdObj>await jsonld.expand(request.body);
-        console.log(expanded[0]);
-        console.log(expanded[0]['http://schema.org/agent'][0]);
-        console.log(expanded[0]['http://schema.org/agent'][0]['http://schema.org/givenName'][0]['@value']);
+        console.log(expanded);
+        const action = expanded[0];
+        console.log(action['http://schema.org/agent'][0]);
+        console.log(action['http://schema.org/agent'][0]['http://schema.org/givenName'][0]['@value']);
+
+        if (!(action['@type'].includes('http://schema.org/RegisterAction'))) {
+            throw new Error(`Not a 'http://schema.org/RegisterAction'`);
+        }
 
         response.status = constants.HTTP_STATUS_OK;
         response.body = await jsonld.compact({}, {});
