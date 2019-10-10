1e7ba81c8f1dfb3131812558c68a2a4d0a01318c
Giorgio Sironi <g.sironi@elifesciences.org>
2019-10-10 11:18:17 +0100

    Implement persistence for RegisterAction

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -16,16 +16,19 @@ const router: Router = new Router();
 
 const actionIriGenerator = (id: string) => `http://localhost:8081${router.url('action', id)}`;
 const articleIriGenerator = (id: string) => `http://localhost:8081${router.url('article', id)}`;
+// doesn't correspond to an actual URL backed by the API at the moment, is that mandatory?
+const userIriGenerator = (id: string) => `http://localhost:8081/user/${id}`;
 
 const actions: Nodes = new FileNodes(path.resolve(__dirname, '../db/actions'), actionIriGenerator);
 const articles: Nodes = new FileNodes(path.resolve(__dirname, '../db/articles'), articleIriGenerator);
+const users: Nodes = new FileNodes(path.resolve(__dirname, '../db/users'), userIriGenerator);
 
 router.get('list', '/', list(articles, router));
 router.get('article', '/articles/:id', article(articles, articleIriGenerator));
 router.get('action', '/actions/:id', action(actions, actionIriGenerator));
 router.post('create-action', '/actions', postAction(actions, articles));
 router.get('search', '/search', search(articles, router));
-router.post('register', '/register', register(articles, router));
+router.post('register', '/register', register(actions, users));
 
 app.use(logger());
 app.use(bodyParser({
--- back-end/src/routes/register.ts
+++ back-end/src/routes/register.ts
@@ -2,8 +2,8 @@ import Koa, {Request} from 'koa';
 import {constants} from 'http2';
 import jsonld from 'jsonld';
 import {Document, JsonLdArray, JsonLdObj} from 'jsonld/jsonld-spec';
+import {DateTime} from 'luxon';
 import {Nodes} from '../nodes';
-import Router from 'koa-router';
 
 // maybe this should be unified with post-action.ts?
 interface RegisterRouteContext extends Koa.Context {
@@ -12,20 +12,38 @@ interface RegisterRouteContext extends Koa.Context {
     },
 }
 
-export default (articles: Nodes, router: Router): Koa.Middleware => {
+export default (actions: Nodes, users: Nodes): Koa.Middleware => {
     return async ({request, response}: RegisterRouteContext): Promise<void> => {
         const expanded = <JsonLdObj>await jsonld.expand(request.body);
-        console.log(expanded);
-        const action = expanded[0];
-        console.log(action['http://schema.org/agent'][0]);
-        console.log(action['http://schema.org/agent'][0]['http://schema.org/givenName'][0]['@value']);
+        let action = expanded[0];
 
         if (!(action['@type'].includes('http://schema.org/RegisterAction'))) {
             throw new Error(`Not a 'http://schema.org/RegisterAction'`);
         }
 
+        // Remove properties we know don't make sense here, allowing ones we don't know about to be kept.
+        // Adapt this from CreateAction to RegisterAction
+        //['http://schema.org/endTime', 'http://schema.org/error', 'http://schema.org/potentialAction', 'http://schema.org/target'].forEach(property => delete action[property]);
+
+        action['http://schema.org/actionStatus'] = 'http://schema.org/ActiveActionStatus';
+        action['http://schema.org/startTime'] = DateTime.utc().toISO();
+
+        await actions.add(action);
+        console.log(action['@id']);
+        const user = action['http://schema.org/agent'][0];
+
+        try {
+            await users.add(user);
+            action['http://schema.org/agent'] = user['@id'];
+            action['http://schema.org/actionStatus'] = 'http://schema.org/CompletedActionStatus';
+        } catch {
+            action['http://schema.org/actionStatus'] = 'http://schema.org/FailedActionStatus';
+        }
+        action['http://schema.org/endTime'] = DateTime.utc().toISO();
+
+        await actions.add(action);
         response.status = constants.HTTP_STATUS_OK;
-        response.body = await jsonld.compact({}, {});
+        response.body = await jsonld.compact(action, {});
         response.type = 'application/ld+json';
     };
 };
