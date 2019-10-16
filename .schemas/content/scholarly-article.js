dce34d832440a51070927d446a2bbae47fb4a37b
Giorgio Sironi <g.sironi@elifesciences.org>
2019-10-16 15:20:35 +0100

    Add Article with authorization-related fields

---
+++ .schemas/content/scholarly-article.js
@@ -0,0 +1,53 @@
+{
+    "@id":"http://localhost:8081/articles/808dce4276ee8ac7464f12ae30c66180",
+    "@type":"http://schema.org/Article",
+    // title of the article, so far so good
+    "http://schema.org/name":"Homo Naledi",
+    // possibly abusing this field as this is not a DigitalDocument,
+    // the common ancestor is CreativeWork
+    "http://schema.org/hasDigitalDocumentPermission": [
+		{
+			"@type": "http://schema.org/DigitalDocumentPermission",
+			"http://schema.org/permissionType": "ReadPermission",
+      // the public and the editorial team can (generically) read
+			"http://schema.org/grantee": [
+				 {
+					"@type": "http://schema.org/Audience",
+					"audienceType": "public"
+				 },
+				 {
+					"@type": "http://schema.org/Audience",
+					"audienceType": "editorial"
+				 }
+			 ]
+		},
+		{
+			"@type": "http://schema.org/DigitalDocumentPermission",
+			"http://schema.org/permissionType": "WritePermission",
+      // the production team can modify
+			"http://schema.org/grantee": [
+				 {
+					"@type": "http://schema.org/Audience",
+					"audienceType": "production"
+				 }
+			 ]
+		}
+	],
+	"http://schema.org/potentialAction": [
+		{
+			"@type": "http://libero.pub/PublishAction",
+			"http://schema.org/agent": [
+        // production team can click "Publish"
+				{
+	  				"@type": "http://schema.org/Role",
+	  				"http://schema.org/roleName": "production"
+				},
+				// editorial team can view the past action(s) but not issue them
+				{
+					  "@type": "http://schema.org/Role",
+            "http://schema.org/roleName": "editorial"
+				}
+			]
+		}
+	]
+}
